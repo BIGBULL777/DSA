be45ea566dc41ab9fb7baaf383fa21acd5c960c3
Kaushik <kaushikp0908@gmail.com>
2025-06-28 15:13:04 +0530

    Add MergeSort implementation

--- src/main/java/org/dsa/MergeSort.java
+++ src/main/java/org/dsa/MergeSort.java
@@ -2,4 +2,32 @@ package org.dsa;
 
 public class MergeSort {
 
+    void MS(int[] array, int low, int high) {
+        if (low == high) return;
+        int mid = (low + high) / 2;
+        MS(array, low, mid);
+        MS(array, mid + 1, high);
+        MergeArray(array, low, mid, high);
+    }
+
+    private void MergeArray(int[] array, int low, int mid, int high) {
+        int[] tempArray = new int[high - low + 1];
+        int tempArrayIndex = 0;
+        int i = 0;
+        int j = mid + 1;
+        while (i <= mid && j <= high){
+            if (array[i] < array[j]){
+                tempArray[tempArrayIndex++] = array[i++];
+            }
+            else {
+                tempArray[tempArrayIndex++] = array[j++];
+            }
+        }
+        while (i < mid){
+            tempArray[tempArrayIndex++] = array[i++];
+        }
+        while (j < high){
+            tempArray[tempArrayIndex++] = array[j++];
+        }
+    }
 }
